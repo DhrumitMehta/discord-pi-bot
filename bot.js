0bb195851878d41c0e24f922b2c42db3446ab6d2
Dhrumit Mehta <dhrumitmehta21@gmail.com>
2020-11-02 13:33:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,6 +60,20 @@ client.on('message', async (message) => {
             const msg = args.join(' ');
             webhookClient.send(msg);
 
+        } else if (CMD_NAME === 'compliment'){
+            numb = Math.floor(Math.random() * Math.floor(4));
+            if (numb == 1){
+                message.channel.send("Your heart is larger than the Milkyway + Andromeda combined!");
+            } else if (numb == 2){
+                message.channel.send("Biology tells you you’re 70% water, Chemistry tells you you’re 60% oxygen, Physics tell you you’re 99.99999999% empty space, And I can tell you you’re 100% cute.");
+            } else if (numb == 3){
+                message.channel.send("Are you French? Because Eiffel for you :)");
+            } else if (numb == 0){
+                message.channel.send("What's the difference betweeen you and practice? Nothing, because both of you make perfect!");
+            } else if (numb == 4){
+                message.channel.send("Aside from being sexy, what do you do for a living?")
+            }
+
         } else if (CMD_NAME === 'roast'){
             number = Math.floor(Math.random() * Math.floor(3));
             if (number == 1){
@@ -69,16 +83,17 @@ client.on('message', async (message) => {
             } else if (number == 3){
                 message.channel.send("You're so single. Nothing else to say");
             } else if (number == 0){
-                message.channel.send("You are so dumb that you thought a laptop is worn by girls");
+                message.channel.send("You are so dumb that you think a laptop is worn by girls");
             }
 
         } else if (CMD_NAME === 'pi'){
-            const pimsg = message.channel.send("What do you want to know about pi?");
+            const pimsg = message.channel.send("What do you want to know about pi? (Reply to this with eiher 1, 2 or 3 to know the corresponding facts!)");
             message.channel.send("1. A brief history of pi.");
             message.channel.send("2. The first 100 decimal places of pi.");
             message.channel.send("3. Some formulas to calculate pi.");
 
             piinfo = true;
+
         } 
     } else if (message.content == '1' && piinfo){
         message.channel.send("Ancient civilizations knew that there was a fixed ratio of circumference to diameter that was approximately equal to three. The Greeks refined the process and Archimedes is credited with the first theoretical calculation of Pi. In 1761 Lambert proved that Pi was irrational, that is, that it can't be written as a ratio of integer numbers. In 1882 Lindeman proved that Pi was transcendental, that is, that Pi is not the root of any algebraic equation with rational coefficients. This discovery proved that you can't 'square a circle', which was a problem that occupied many mathematicians up to that time.")
@@ -88,6 +103,8 @@ client.on('message', async (message) => {
         piinfo = false;
     } else if (message.content == '3' && piinfo){
         message.channel.send("Euler's Formula : (Pi^2)/6 = SUM (n = 1..infinity) of 1/n2 = 1/12 + 1/22 + 1/32 + ...");
+        message.channel.send("Leibnitz's Formula : Pi/4 = 1/1 - 1/3 + 1/5 - 1/7 + ...");
+        message.channel.send("Wallis Product : Pi/2 = 2/1 * 2/3 * 4/3 * 4/5 * 6/5 * 6/7 * ...");
         piinfo = false;
     }
 });
